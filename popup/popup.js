1240265c4162cd8482ae4e5c23509ee9f8a7775f
Jannat <2202jannat@gmail.com>
2024-07-06 11:43:09 +0600

    Add files via upload

---
+++ popup/popup.js
@@ -0,0 +1,13 @@
+
+let pop = document.getElementById("popup");
+
+function openpopup() {
+    pop.classList.add("active");
+
+}
+
+function closepopup() {
+    pop.classList.remove("active");
+}
+
+
